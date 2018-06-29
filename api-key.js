68ef7592bf67fc54f31f313fc3d69bea7961a84f
elliotwaite <1767836+elliotwaite@users.noreply.github.com>
2018-06-29 13:26:34 -1000

    Update API key info.

--- api-key.js
+++ api-key.js
@@ -1,1 +1,19 @@
-const YOUTUBE_API_KEY = 'the-private-youtube-data-api-key-goes-here'
+// If you'd like to help develop this extension, you'll need to get a
+// YouTube Data API key to run this Chrome extension locally. To get one:
+//
+// 1. Sign up for a Google Cloud account:
+//    https://cloud.google.com
+//
+// 2. Go to the YouTube Data API v3 page:
+//    https://console.cloud.google.com/apis/library/youtube.googleapis.com
+//
+// 3. Click the "ENABLE" button to enable the API.
+//
+// 4. Go to the API Credentials page:
+//    https://console.cloud.google.com/apis/credentials
+//
+// 5. Click the "Create credentials" dropdown button, then choose "API key".
+//
+// 6. Then you'll see an API key that you can copy and paste here.
+
+const YOUTUBE_API_KEY = 'paste-your-youtube-data-api-key-here'
