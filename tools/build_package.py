e90889b1d3d172e3bd76b4a7138c438a0044e9fd
elliotwaite <1767836+elliotwaite@users.noreply.github.com>
2019-05-02 12:26:05 -1000

    Add TODO to build script related to Firefox background scripts.

--- tools/build_package.py
+++ tools/build_package.py
@@ -1,16 +1,53 @@
 """Build the package.zip file that will be uploaded to the Chrome Web Store."""
 
 import os
+import shutil
 import zipfile
 
 EXTENSION_DIR = '../extension/'
-EXCLUDED_FILENAMES = ['.DS_Store']
 
-with zipfile.ZipFile('package.zip', 'w') as z:
-  for dir_path, dir_names, filenames in os.walk(EXTENSION_DIR):
-    for filename in filenames:
-      if filename not in EXCLUDED_FILENAMES:
+
+def zip_dir(source_dir, output_path, excluded_filenames=None):
+  if excluded_filenames is None:
+    excluded_filenames = ['.DS_Store']
+  print(f'Zipping: {source_dir} --> {output_path}')
+  with zipfile.ZipFile(output_path, 'w') as z:
+    for dir_path, dir_names, filenames in os.walk(source_dir):
+      for filename in filenames:
+        if filename in excluded_filenames:
+          continue
         path = os.path.join(dir_path, filename)
-        path_in_zip = path[len(EXTENSION_DIR):]
+        path_in_zip = path[len(source_dir):]
         print(path_in_zip)
         z.write(path, path_in_zip)
+  print()
+
+
+def main():
+  # Build the Chrome package.
+  zip_dir(EXTENSION_DIR, 'chrome-package.zip')
+
+  # Build the Firefox package (removing the `"persistent": false` line from the
+  # manifest.json file, since Firefox does not support persistent background
+  # scripts).
+  # TODO: remove this separate build process once Firefox supports
+  #       nonpersistent background scripts.
+  #       https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/manifest.json/background
+  tmp_dir = 'tmp'
+  shutil.copytree(EXTENSION_DIR, tmp_dir)
+  manifest_path = os.path.join(tmp_dir, 'manifest.json')
+  with open(manifest_path, 'r') as f:
+    text = f.read()
+  text = text.replace(',\n    "persistent": false', '')
+  assert text.find('persistent') == -1, (
+      'Persistent background scripts are not supported by Firefox, however, '
+      'the word "persistent" was found in the manifest.json file for the '
+      'Firefox package.')
+  with open(manifest_path, 'w') as f:
+    f.write(text)
+  zip_dir(tmp_dir, 'firefox-package.zip')
+  shutil.rmtree(tmp_dir)
+
+
+if __name__ == '__main__':
+  main()
