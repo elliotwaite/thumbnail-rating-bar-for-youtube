f1ec4a2a345aac390226e488e5c899d7f940c1b3
elliotwaite <1767836+elliotwaite@users.noreply.github.com>
2020-09-24 15:56:24 -0700

    Change the "chrome_style" key to "browser_style" for Firefox

--- tools/build_package.py
+++ tools/build_package.py
@@ -1,5 +1,8 @@
-"""Build the extension (create the package.zip file) for Chrome and Firefox."""
+"""Build the extension.
 
+Creates the chrome-package.zip file for Chrome, and the
+firefox-package.zip file for Firefox.
+"""
 import os
 import shutil
 import zipfile
@@ -8,46 +11,65 @@ EXTENSION_DIR = '../extension/'
 
 
 def zip_dir(source_dir, output_path, excluded_filenames=None):
-  if excluded_filenames is None:
-    excluded_filenames = ['.DS_Store']
-  print(f'Zipping: {source_dir} --> {output_path}')
-  with zipfile.ZipFile(output_path, 'w') as z:
-    for dir_path, dir_names, filenames in os.walk(source_dir):
-      for filename in filenames:
-        if filename in excluded_filenames:
-          continue
-        path = os.path.join(dir_path, filename)
-        path_in_zip = path[len(source_dir):]
-        print(path_in_zip)
-        z.write(path, path_in_zip)
-  print()
+    if excluded_filenames is None:
+        excluded_filenames = ['.DS_Store']
+    print(f'Zipping: {source_dir} --> {output_path}')
+    with zipfile.ZipFile(output_path, 'w') as z:
+        for dir_path, dir_names, filenames in os.walk(source_dir):
+            for filename in filenames:
+                if filename in excluded_filenames:
+                    continue
+                path = os.path.join(dir_path, filename)
+                path_in_zip = path[len(source_dir) :]
+                print(path_in_zip)
+                z.write(path, path_in_zip)
+    print()
+
+
+def build_chrome_package():
+    zip_dir(EXTENSION_DIR, 'chrome-package.zip')
+
+
+def build_firefox_package():
+    tmp_dir = 'tmp'
+    shutil.copytree(EXTENSION_DIR, tmp_dir)
+    manifest_path = os.path.join(tmp_dir, 'manifest.json')
+    with open(manifest_path, 'r') as f:
+        manifest_text = f.read()
+
+    # Removing the `"persistent": false` line from the manifest.json file,
+    # since Firefox does not support persistent background scripts).
+    #
+    # TODO: Remove this step once Firefox supports non-persistent
+    # background scripts.
+    # https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/manifest.json/background
+    manifest_text = manifest_text.replace(',\n    "persistent": false', '')
+    assert manifest_text.find('persistent') == -1, (
+        'Persistent background scripts are not supported by Firefox, however, '
+        'the word "persistent" was found in the manifest.json file for the '
+        'Firefox package.'
+    )
+
+    # Change the "chrome_style" key to "browser_style". They are
+    # equivalent, but Firefox uses the "browser_style" key instead.
+    # https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/user_interface/Browser_styles
+    manifest_text = manifest_text.replace('"chrome_style": false', '"browser_style": false')
+    assert manifest_text.find('chrome_style') == -1, (
+        'The "chrome_style" key is not supported by Firefox, however,the '
+        'word "chrome_style" was found in the manifest.json file for the '
+        'Firefox package.'
+    )
+
+    with open(manifest_path, 'w') as f:
+        f.write(manifest_text)
+    zip_dir(tmp_dir, 'firefox-package.zip')
+    shutil.rmtree(tmp_dir)
 
 
 def main():
-  # Build the Chrome package.
-  zip_dir(EXTENSION_DIR, 'chrome-package.zip')
-
-  # TODO: Remove this separate build process once Firefox supports
-  #       nonpersistent background scripts.
-  #       https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/manifest.json/background
-  # Build the Firefox package (removing the `"persistent": false` line from the
-  # manifest.json file, since Firefox does not support persistent background
-  # scripts).
-  tmp_dir = 'tmp'
-  shutil.copytree(EXTENSION_DIR, tmp_dir)
-  manifest_path = os.path.join(tmp_dir, 'manifest.json')
-  with open(manifest_path, 'r') as f:
-    text = f.read()
-  text = text.replace(',\n    "persistent": false', '')
-  assert text.find('persistent') == -1, (
-      'Persistent background scripts are not supported by Firefox, however, '
-      'the word "persistent" was found in the manifest.json file for the '
-      'Firefox package.')
-  with open(manifest_path, 'w') as f:
-    f.write(text)
-  zip_dir(tmp_dir, 'firefox-package.zip')
-  shutil.rmtree(tmp_dir)
+    build_chrome_package()
+    build_firefox_package()
 
 
 if __name__ == '__main__':
-  main()
+    main()
