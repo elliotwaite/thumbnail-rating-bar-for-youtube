2d187530891e83cb6d8f852528486d9597600f51
elliotwaite <1767836+elliotwaite@users.noreply.github.com>
2021-01-30 05:58:11 -0800

    Update package builder

--- tools/build_package.py
+++ tools/build_package.py
@@ -1,17 +1,14 @@
-"""Build the extension.
-
-Creates the chrome-package.zip file for Chrome, and the
-firefox-package.zip file for Firefox.
-"""
+"""Builds the extension for multiple browsers."""
 import os
 import shutil
+import tempfile
 import zipfile
 
 CUR_DIR = os.path.join(os.path.dirname(__file__))
 BUILD_DIR = os.path.abspath(os.path.join(CUR_DIR, '../build/'))
-CHROME_PACKAGE_PATH = os.path.join(BUILD_DIR, 'chrome-package.zip')
-FIREFOX_PACKAGE_PATH = os.path.join(BUILD_DIR, 'firefox-package.zip')
 EXTENSION_DIR = os.path.abspath(os.path.join(CUR_DIR, '../extension/'))
+VALID_BROWSERS = 'chrome', 'firefox', 'edge', 'opera', 'safari', 'firefox_android'
+TARGET_BROWSERS = 'chrome', 'firefox', 'edge'
 
 
 def zip_dir(source_dir, output_path, excluded_filenames=None):
@@ -32,53 +29,51 @@ def zip_dir(source_dir, output_path, excluded_filenames=None):
     print()
 
 
-def build_chrome_package():
-    zip_dir(EXTENSION_DIR, CHROME_PACKAGE_PATH)
+def build_package(browser):
+    assert browser in VALID_BROWSERS, 'Unsupported browser specified.'
+    package_path = os.path.join(BUILD_DIR, f'{browser}-package.zip')
 
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        tmp_extension_dir = os.path.join(tmp_dir, 'extension')
+        shutil.copytree(EXTENSION_DIR, tmp_extension_dir)
+        manifest_path = os.path.join(tmp_extension_dir, 'manifest.json')
+        with open(manifest_path, 'r') as f:
+            manifest_text = f.read()
 
-def build_firefox_package():
-    # This separate build is required to customize the manifest.json
-    # file for Firefox.
-    tmp_dir = 'tmp'
-    shutil.copytree(EXTENSION_DIR, tmp_dir)
-    manifest_path = os.path.join(tmp_dir, 'manifest.json')
-    with open(manifest_path, 'r') as f:
-        manifest_text = f.read()
+        if browser in ('firefox', 'safari', 'firefox_android'):
+            # Remove the `"persistent": false` line from the manifest.json file
+            # for browsers that do not support persistent background scripts.
+            # https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/manifest.json/background
+            manifest_text = manifest_text.replace(',\n    "persistent": false', '')
+            assert manifest_text.find('persistent') == -1, (
+                'Persistent background scripts are not supported by Firefox, however, '
+                'the word "persistent" was found in the manifest.json file for the '
+                'Firefox package.'
+            )
 
-    # Removing the `"persistent": false` line from the manifest.json file,
-    # since Firefox does not support persistent background scripts).
-    #
-    # TODO: Remove this step once Firefox supports non-persistent
-    # background scripts.
-    # https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/manifest.json/background
-    manifest_text = manifest_text.replace(',\n    "persistent": false', '')
-    assert manifest_text.find('persistent') == -1, (
-        'Persistent background scripts are not supported by Firefox, however, '
-        'the word "persistent" was found in the manifest.json file for the '
-        'Firefox package.'
-    )
+        if browser not in ('chrome', 'opera'):
+            # Change the "chrome_style" key to "browser_style". They are
+            # equivalent, but some browsers use the "browser_style"
+            # key instead.
+            # https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/user_interface/Browser_styles
+            manifest_text = manifest_text.replace('"chrome_style": false', '"browser_style": false')
+            assert manifest_text.find('chrome_style') == -1, (
+                'The "chrome_style" key is not supported by Firefox, however,the '
+                'word "chrome_style" was found in the manifest.json file for the '
+                'Firefox package.'
+            )
 
-    # Change the "chrome_style" key to "browser_style". They are
-    # equivalent, but Firefox uses the "browser_style" key instead.
-    # https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/user_interface/Browser_styles
-    manifest_text = manifest_text.replace('"chrome_style": false', '"browser_style": false')
-    assert manifest_text.find('chrome_style') == -1, (
-        'The "chrome_style" key is not supported by Firefox, however,the '
-        'word "chrome_style" was found in the manifest.json file for the '
-        'Firefox package.'
-    )
+        with open(manifest_path, 'w') as f:
+            f.write(manifest_text)
 
-    with open(manifest_path, 'w') as f:
-        f.write(manifest_text)
-    zip_dir(tmp_dir, FIREFOX_PACKAGE_PATH)
-    shutil.rmtree(tmp_dir)
+        zip_dir(tmp_extension_dir, package_path)
+        print(f'Generated package: {package_path}\n')
 
 
 def main():
     os.makedirs(BUILD_DIR, exist_ok=True)
-    build_chrome_package()
-    build_firefox_package()
-    print(f'Generated package files:\n  {CHROME_PACKAGE_PATH}\n  {FIREFOX_PACKAGE_PATH}')
+    for browser in TARGET_BROWSERS:
+        build_package(browser)
 
 
 if __name__ == '__main__':
